87685321ab4a4cae72a5ed7dca388d0990b6d0bf
Tiddo Langerak <tiddolangerak@gmail.com>
2015-01-14 14:55:47 +0100

    Prevent head request from being done on disabled domains

--- build/angular-retina.js
+++ build/angular-retina.js
@@ -1,12 +1,12 @@
-/*! angular-retina - v0.3.1 - 2014-06-30
+/*! angular-retina - v0.3.1 - 2015-01-14
 * https://github.com/jrief/angular-retina
-* Copyright (c) 2014 Jacob Rief; Licensed MIT */
+* Copyright (c) 2015 Jacob Rief; Licensed MIT */
 // Add support for Retina displays when using element attribute "ng-src".
 // This module overrides the built-in directive "ng-src" with one which
 // distinguishes between standard or high-resolution (Retina) displays.
 (function (angular, undefined) {
   'use strict';
-  var infix = '@2x', data_url_regex = /^data:([a-z]+\/[a-z]+(;[a-z\-]+\=[a-z\-]+)?)?(;base64)?,(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/i;
+  var infix = '@2x', data_url_regex = /^data:([a-z]+\/[a-z]+(;[a-z\-]+\=[a-z\-]+)?)?(;base64)?,(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/, pattern = /.*/;
   var ngRetina = angular.module('ngRetina', []).config([
       '$provide',
       function ($provide) {
@@ -24,6 +24,9 @@
     this.setInfix = function (value) {
       infix = value;
     };
+    this.setPattern = function (value) {
+      pattern = new RegExp(value);
+    };
     this.$get = angular.noop;
   });
   ngRetina.directive('ngSrc', [
@@ -38,11 +41,14 @@
           return $window.matchMedia && $window.matchMedia(mediaQuery).matches;
         }();
       function getHighResolutionURL(url) {
-        var parts = url.split('.');
-        if (parts.length < 2)
-          return url;
-        parts[parts.length - 2] += infix;
-        return parts.join('.');
+        var pathParts = url.split('/');
+        var file = pathParts.pop();
+        var fileParts = file.split('.');
+        if (fileParts.length < 2)
+          return url + infix;
+        fileParts[fileParts.length - 2] += infix;
+        pathParts.push(fileParts.join('.'));
+        return pathParts.join('/');
       }
       return function (scope, element, attrs) {
         function setImgSrc(img_url) {
@@ -51,6 +57,10 @@
             element.prop('src', img_url);
         }
         function set2xVariant(img_url) {
+          if (pattern.test(img_url)) {
+            setImgSrc(img_url);
+            return;
+          }
           var img_url_2x = $window.sessionStorage.getItem(img_url);
           if (!img_url_2x) {
             img_url_2x = getHighResolutionURL(img_url);
